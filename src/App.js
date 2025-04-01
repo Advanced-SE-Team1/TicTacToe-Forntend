c20b9daad2a731a80e684dd5ec6ed25e3643831a
BuddhikaDev96 <buddhikadev96@gmail.com>
2025-04-01 22:01:51 +0530

    base project and App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Game from "./pages/Game";
+// import { GameProvider } from "./context/GameContext";
+import "./styles/App.css";
+
+function App() {
+  return (
+    // <GameProvider>
+    //   <div className="App">
+    //     <Game />
+    //   </div>
+    // </GameProvider>
+  );
+}
+
+export default App;
